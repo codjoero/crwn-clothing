c216fa132bcad3d6dab0180ca30ea9f2505b3a34
Ronald Nsereko <rownee2001@gmail.com>
2019-08-30 17:02:17 +0300

    add files for homepage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import './App.css';
+import HomePage from './pages/homepage/homepage';
 
 function App() {
   return (
-    <div>Hello world</div>
+    <HomePage />
   );
 }
 
